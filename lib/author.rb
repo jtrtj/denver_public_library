2df62bad42420807fb7575b937ffe821d31380a7
JohnRoemer <jt.r@live.com>
2018-05-24 09:35:03 -0600

    starts author class and test

--- lib/author.rb
+++ lib/author.rb
@@ -1,3 +1,8 @@
 class Author
+    attr_reader :books
 
+  def initialize(info)
+    @info = info
+    @books = []
+  end
 end
--- test/author_test.rb
+++ test/author_test.rb
@@ -1,6 +1,22 @@
 require './test/test_helper'
+require './lib/author.rb'
 
 class AuthorTest < Minitest::Test
+  def setup
+    author_info = {first_name: "Charlotte", last_name: "Bronte"}
+    @charlotte_bronte = Author.new(author_info)
+  end
 
+  def test_it_exists
+    assert_instance_of Author, @charlotte_bronte
+  end
+
+  def test_author_starts_with_no_books
+    assert_equal [], @charlotte_bronte.books
+  end
+
+  def test_books_can_be_added_to_author
+    charlotte_bronte.add_book("Jane Eyre", "October 16, 1847")
+  end
 
 end
