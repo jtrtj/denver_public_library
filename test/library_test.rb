beb70bda42c16e0baea9743ba29581f766740d40
JohnRoemer <jt.r@live.com>
2018-05-24 10:09:03 -0600

    starts library class and test

--- test/library_test.rb
+++ test/library_test.rb
@@ -1,6 +1,13 @@
 require './test/test_helper'
+require './lib/library.rb'
+require './lib/author.rb'
+require './lib/book.rb'
 
 class LibraryTest < Minitest::Test
+  def test_it_exisits
+    dpl = Library.new
 
+    assert_instance_of Library, dpl
+  end
 
 end
